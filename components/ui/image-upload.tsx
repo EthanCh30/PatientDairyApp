d9bbfc9ad0623ee461c6582f63eb7f0b6a84902f
chen <565306120@qq.com>
2024-07-21 01:51:33 +1000

    update image-upload

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="bznt77qm">
         {({ open }) => {
           const onClick = () => {
             open();
